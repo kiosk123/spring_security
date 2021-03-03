7209a25671847eeea9ac73f7e44a23ba9c22d48c
Heo Jong Tae <heo9910@naver.com>
2021-03-03 19:34:52 +0900

    .

--- chapter-01/src/main/java/study/SecurityController.java
+++ chapter-01/src/main/java/study/SecurityController.java
@@ -4,15 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Securityontroller {
+public class SecurityController {
 
     @GetMapping("/")
     public String index() {
         return "home";
-    }
-
-    @GetMapping("/loginPage")
-    public String loginPage() {
-        return "loginPage";
     }
 }
--- chapter-03/src/main/java/study/SecurityController.java
+++ chapter-03/src/main/java/study/SecurityController.java
@@ -4,10 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class IndexController {
+public class SecurityController {
 
     @GetMapping("/")
     public String index() {
         return "home";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
     }
 }
