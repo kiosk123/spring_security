4ae40efe84d9f01779b355665a08f348cd69110d
Heo Jong Tae <heo9910@naver.com>
2021-05-13 14:48:55 +0900

    .

--- chapter-26/src/main/java/io/domain/dto/AccountDto.java
+++ chapter-26/src/main/java/io/domain/dto/AccountDto.java
@@ -1,10 +1,15 @@
 package io.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Builder
-@Getter
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AccountDto {
     String username;
     private String password;
